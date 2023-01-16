555cdfbabda6b0083e51776f581959df4ccb083c
LaryIsland <larytreadway@gmail.com>
2023-01-16 17:02:56 +0000

    Add portal, spyglass, and vignette overlay options
    Fix translation text error
    Bump version to 0.2.0

--- src/main/java/com/laryisland/screenfx/config/ScreenFXConfig.java
+++ src/main/java/com/laryisland/screenfx/config/ScreenFXConfig.java
@@ -18,4 +18,20 @@ public class ScreenFXConfig extends MidnightConfig {
   public static Comment configUnderwaterSettings;
   @Entry(name = "screenfx.midnightconfig.configUnderwaterOpacity", isSlider = true, min = 0f, max = 1f)
   public static float underwaterOpacity = 0.1f;
+  @Comment(centered = true)
+  public static Comment configPortalSettings;
+  @Entry(name = "screenfx.midnightconfig.configPortalOpacity", isSlider = true, min = 0f, max = 1f)
+  public static float portalOpacity = 1f;
+  @Comment(centered = true)
+  public static Comment configSpyglassSettings;
+  @Entry(name = "screenfx.midnightconfig.configSpyglassTextureOpacity", isSlider = true, min = 0f, max = 1f)
+  public static float spyglassTextureOpacity = 1f;
+  @Entry(name = "screenfx.midnightconfig.configSpyglassOverlayOpacity", isSlider = true, min = 0, max = 255)
+  public static int spyglassOverlayOpacity = 255;
+  @Comment(centered = true)
+  public static Comment configVignetteSettings;
+  @Entry(name = "screenfx.midnightconfig.configVignetteOpacity", isSlider = true, min = 0f, max = 1f)
+  public static float vignetteOpacity = 1f;
+  @Entry(name = "screenfx.midnightconfig.configVignetteColour", isColor = true)
+  public static String vignetteColour = "#000000";
 }
