39c0ed8e3893a6f9160ca47de338eb8a90e8c0f1
LaryIsland <larytreadway@gmail.com>
2023-02-25 20:30:16 +0000

    Split held item options between blocks and items
    Bump version to 1.2.0

--- src/main/java/com/laryisland/screenfx/config/ScreenFXConfig.java
+++ src/main/java/com/laryisland/screenfx/config/ScreenFXConfig.java
@@ -94,6 +94,39 @@ public class ScreenFXConfig extends MidnightConfig {
 	public static boolean totemOfUndyingDisable = false;
 
 	@Comment(centered = true)
+	public static Comment heldBlockSettings;
+	@Entry(isSlider = true, min = -180f, max = 180f, precision = 1)
+	public static float heldBlockMainHandRotationAxisX = 0f;
+	@Entry(isSlider = true, min = -180f, max = 180f, precision = 1)
+	public static float heldBlockMainHandRotationAxisY = 0f;
+	@Entry(isSlider = true, min = -180f, max = 180f, precision = 1)
+	public static float heldBlockMainHandRotationAxisZ = 0f;
+	@Entry(isSlider = true, min = 0f, max = 2f)
+	public static float heldBlockMainHandScale = 1f;
+	@Entry(isSlider = true, min = -1f, max = 1f)
+	public static float heldBlockMainHandTranslationAxisX = 0f;
+	@Entry(isSlider = true, min = -1f, max = 1f)
+	public static float heldBlockMainHandTranslationAxisY = 0f;
+	@Entry(isSlider = true, min = -1f, max = 1f)
+	public static float heldBlockMainHandTranslationAxisZ = 0f;
+	@Entry
+	public static boolean heldBlockOffhandMirrorsMainHand = true;
+	@Entry(isSlider = true, min = -180f, max = 180f, precision = 1)
+	public static float heldBlockOffhandRotationAxisX = 0f;
+	@Entry(isSlider = true, min = -180f, max = 180f, precision = 1)
+	public static float heldBlockOffhandRotationAxisY = 0f;
+	@Entry(isSlider = true, min = -180f, max = 180f, precision = 1)
+	public static float heldBlockOffhandRotationAxisZ = 0f;
+	@Entry(isSlider = true, min = 0f, max = 2f)
+	public static float heldBlockOffhandScale = 1f;
+	@Entry(isSlider = true, min = -1f, max = 1f)
+	public static float heldBlockOffhandTranslationAxisX = 0f;
+	@Entry(isSlider = true, min = -1f, max = 1f)
+	public static float heldBlockOffhandTranslationAxisY = 0f;
+	@Entry(isSlider = true, min = -1f, max = 1f)
+	public static float heldBlockOffhandTranslationAxisZ = 0f;
+
+	@Comment(centered = true)
 	public static Comment heldItemSettings;
 	@Entry(isSlider = true, min = -180f, max = 180f, precision = 1)
 	public static float heldItemMainHandRotationAxisX = 0f;
@@ -109,10 +142,8 @@ public class ScreenFXConfig extends MidnightConfig {
 	public static float heldItemMainHandTranslationAxisY = 0f;
 	@Entry(isSlider = true, min = -1f, max = 1f)
 	public static float heldItemMainHandTranslationAxisZ = 0f;
-
 	@Entry
 	public static boolean heldItemOffhandMirrorsMainHand = true;
-	
 	@Entry(isSlider = true, min = -180f, max = 180f, precision = 1)
 	public static float heldItemOffhandRotationAxisX = 0f;
 	@Entry(isSlider = true, min = -180f, max = 180f, precision = 1)
--- src/main/java/com/laryisland/screenfx/mixin/HeldItemRendererMixin.java
+++ src/main/java/com/laryisland/screenfx/mixin/HeldItemRendererMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.RotationAxis;
@@ -30,24 +31,56 @@ public class HeldItemRendererMixin {
 	private void renderHeldItem_matrixManipulation(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand,
 			float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices,
 			VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
-		if (hand == Hand.MAIN_HAND) {
-			matrices.translate(ScreenFXConfig.heldItemMainHandTranslationAxisX, ScreenFXConfig.heldItemMainHandTranslationAxisY, ScreenFXConfig.heldItemMainHandTranslationAxisZ);
-			matrices.scale(ScreenFXConfig.heldItemMainHandScale, ScreenFXConfig.heldItemMainHandScale, ScreenFXConfig.heldItemMainHandScale);
-			matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(ScreenFXConfig.heldItemMainHandRotationAxisX));
-			matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(ScreenFXConfig.heldItemMainHandRotationAxisY));
-			matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(ScreenFXConfig.heldItemMainHandRotationAxisZ));
-		} else if (ScreenFXConfig.heldItemOffhandMirrorsMainHand) {
-			matrices.translate(-ScreenFXConfig.heldItemMainHandTranslationAxisX, ScreenFXConfig.heldItemMainHandTranslationAxisY, ScreenFXConfig.heldItemMainHandTranslationAxisZ);
-			matrices.scale(ScreenFXConfig.heldItemMainHandScale, ScreenFXConfig.heldItemMainHandScale, ScreenFXConfig.heldItemMainHandScale);
-			matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(ScreenFXConfig.heldItemMainHandRotationAxisX));
-			matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-ScreenFXConfig.heldItemMainHandRotationAxisY));
-			matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(-ScreenFXConfig.heldItemMainHandRotationAxisZ));
+		float transX, transY, transZ, scale, rotX, rotY, rotZ;
+		if (Item.BLOCK_ITEMS.containsValue(item.getItem())) {
+			transY = ScreenFXConfig.heldBlockMainHandTranslationAxisY;
+			transZ = ScreenFXConfig.heldBlockMainHandTranslationAxisZ;
+			scale = ScreenFXConfig.heldBlockMainHandScale;
+			rotX = ScreenFXConfig.heldBlockMainHandRotationAxisX;
+			if (hand == Hand.MAIN_HAND) {
+				transX = ScreenFXConfig.heldBlockMainHandTranslationAxisX;
+				rotY = ScreenFXConfig.heldBlockMainHandRotationAxisY;
+				rotZ = ScreenFXConfig.heldBlockMainHandRotationAxisZ;
+			} else if (ScreenFXConfig.heldBlockOffhandMirrorsMainHand) {
+				transX = -ScreenFXConfig.heldBlockMainHandTranslationAxisX;
+				rotY = -ScreenFXConfig.heldBlockMainHandRotationAxisY;
+				rotZ = -ScreenFXConfig.heldBlockMainHandRotationAxisZ;
+			} else {
+				transX = ScreenFXConfig.heldBlockOffhandTranslationAxisX;
+				transY = ScreenFXConfig.heldBlockOffhandTranslationAxisY;
+				transZ = ScreenFXConfig.heldBlockOffhandTranslationAxisZ;
+				scale = ScreenFXConfig.heldBlockOffhandScale;
+				rotX = ScreenFXConfig.heldBlockOffhandRotationAxisX;
+				rotY = ScreenFXConfig.heldBlockOffhandRotationAxisY;
+				rotZ = ScreenFXConfig.heldBlockOffhandRotationAxisZ;
+			}
 		} else {
-			matrices.translate(ScreenFXConfig.heldItemOffhandTranslationAxisX, ScreenFXConfig.heldItemOffhandTranslationAxisY, ScreenFXConfig.heldItemOffhandTranslationAxisZ);
-			matrices.scale(ScreenFXConfig.heldItemOffhandScale, ScreenFXConfig.heldItemOffhandScale, ScreenFXConfig.heldItemOffhandScale);
-			matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(ScreenFXConfig.heldItemOffhandRotationAxisX));
-			matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(ScreenFXConfig.heldItemOffhandRotationAxisY));
-			matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(ScreenFXConfig.heldItemOffhandRotationAxisZ));
+			transY = ScreenFXConfig.heldItemMainHandTranslationAxisY;
+			transZ = ScreenFXConfig.heldItemMainHandTranslationAxisZ;
+			scale = ScreenFXConfig.heldItemMainHandScale;
+			rotX = ScreenFXConfig.heldItemMainHandRotationAxisX;
+			if (hand == Hand.MAIN_HAND) {
+				transX = ScreenFXConfig.heldItemMainHandTranslationAxisX;
+				rotY = ScreenFXConfig.heldItemMainHandRotationAxisY;
+				rotZ = ScreenFXConfig.heldItemMainHandRotationAxisZ;
+			} else if (ScreenFXConfig.heldItemOffhandMirrorsMainHand) {
+				transX = -ScreenFXConfig.heldItemMainHandTranslationAxisX;
+				rotY = -ScreenFXConfig.heldItemMainHandRotationAxisY;
+				rotZ = -ScreenFXConfig.heldItemMainHandRotationAxisZ;
+			} else {
+				transX = ScreenFXConfig.heldItemOffhandTranslationAxisX;
+				transY = ScreenFXConfig.heldItemOffhandTranslationAxisY;
+				transZ = ScreenFXConfig.heldItemOffhandTranslationAxisZ;
+				scale = ScreenFXConfig.heldItemOffhandScale;
+				rotX = ScreenFXConfig.heldItemOffhandRotationAxisX;
+				rotY = ScreenFXConfig.heldItemOffhandRotationAxisY;
+				rotZ = ScreenFXConfig.heldItemOffhandRotationAxisZ;
+			}
 		}
+		matrices.translate(transX, transY, transZ);
+		matrices.scale(scale, scale, scale);
+		matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(rotX));
+		matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(rotY));
+		matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(rotZ));
 	}
 }
